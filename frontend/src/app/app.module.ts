0f517d7a655f2d87b6f4ce168ca8cbc25ac1ff03
neha5444 <neha.limaje@solacetechnologies.co.in>
2023-10-10 17:22:39 +0530

    login logout functionality

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,7 +13,8 @@ import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -31,9 +32,15 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut : 3000,
+      positionClass : 'toast-bottom-right',
+      newestOnTop  : false
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/share/constants/urls.ts
+++ frontend/src/app/share/constants/urls.ts
@@ -5,3 +5,5 @@ export const FOOD_TAGS_URL = FOOD_URL + '/tags';
 export const FOOD_BY_SEARCH_URL = FOOD_URL + '/search/';
 export const FOOD_BY_TAGS_URL = FOOD_URL + '/tag/';
 export const FOOD_BY_ID_URL = FOOD_URL + '/';
+
+export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
